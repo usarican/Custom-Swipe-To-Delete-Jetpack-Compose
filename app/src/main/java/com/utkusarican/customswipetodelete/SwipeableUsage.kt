b5e3bfe6b75255a8ab1e17d9325c3e543e1c00d9
ibrahimutkusarican <iu.sarican@gmail.com>
2024-09-13 10:12:11 +0300

    Devamke

--- app/src/main/java/com/utkusarican/customswipetodelete/SwipeableUsage.kt
+++ app/src/main/java/com/utkusarican/customswipetodelete/SwipeableUsage.kt
@@ -1,47 +1,43 @@
 package com.utkusarican.customswipetodelete
 
 import android.util.Log
-import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.End
-import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Start
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.AnchoredDraggableState
 import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
-import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxDefaults
 import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -100,54 +96,12 @@ fun SwipeToDismissListItems(
 
 enum class DragAnchors {
     Start,
+    MediumOne,
+    MediumTwo,
+    MediumThree,
     End,
 }
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun AnchoredDraggableBox(
-    modifier: Modifier = Modifier
-) {
-    val localDensity = LocalDensity.current
-
-    val decayAnimation = rememberSplineBasedDecay<Float>()
-
-    val state = remember {
-        AnchoredDraggableState(
-            initialValue = DragAnchors.Start,
-            positionalThreshold = { distance: Float -> distance * 0.5f },
-            velocityThreshold = { with(localDensity) { 100.dp.toPx() } },
-            snapAnimationSpec = tween(),
-            decayAnimationSpec = decayAnimation
-        ).apply {
-            updateAnchors(
-                DraggableAnchors {
-                    DragAnchors.Start at 0f
-                    DragAnchors.End at 400f
-                }
-            )
-        }
-    }
-    Log.d("AnchoredDraggableBox", "Offset: ${state.requireOffset()}")
-    Box(
-        Modifier
-            .size(48.dp)
-            .background(Color.Red)
-            .offset {
-                IntOffset(
-                    // 2
-                    x = state
-                        .requireOffset()
-                        .roundToInt(),
-                    y = 0,
-                )
-            }
-            .anchoredDraggable(
-                state = state,
-                orientation = Orientation.Horizontal
-            )
-    )
-}
 
 @Composable
 @OptIn(ExperimentalFoundationApi::class)
@@ -156,30 +110,36 @@ fun HorizontalDraggableSample(
 ) {
     val density = LocalDensity.current
     val decayAnimation = rememberSplineBasedDecay<Float>()
+    var totalDistance by remember { mutableFloatStateOf(0f) }
     val state = remember {
         AnchoredDraggableState(
             initialValue = DragAnchors.Start,
-            positionalThreshold = { distance: Float -> distance * 0.5f },
-            velocityThreshold = { with(density) { 100.dp.toPx() } },
+            positionalThreshold = { distance: Float ->
+                distance},
+            velocityThreshold = { with(density) { 200.dp.toPx() } },
             snapAnimationSpec = tween(),
             decayAnimationSpec = decayAnimation
         ).apply {
             updateAnchors(
                 DraggableAnchors {
                     DragAnchors.Start at 0f
-                    DragAnchors.End at 400f
+                    DragAnchors.MediumOne at 200f
+                    DragAnchors.MediumTwo at 400f
+                    DragAnchors.MediumThree at 600f
+                    DragAnchors.End at totalDistance
                 }
             )
         }
     }
 
+    Log.d("AnchoredDraggableBox", "Offset: ${state.currentValue}")
     Box(
         modifier = modifier,
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
+        Card(
             modifier = Modifier
-                .size(80.dp)
+                .height(80.dp)
+                .fillMaxWidth()
                 .offset {
                     IntOffset(
                         x = state
@@ -188,8 +148,14 @@ fun HorizontalDraggableSample(
                     )
                 }
                 .anchoredDraggable(state, Orientation.Horizontal),
-            contentDescription = null,
-        )
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Blue
+            )
+        ){
+            Box(modifier = Modifier
+                .size(64.dp)
+                .background(Color.LightGray))
+        }
     }
 }
 
